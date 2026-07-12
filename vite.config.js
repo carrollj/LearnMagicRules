42cc9b2fc2c4ea67889c1f08307364e9ddcf8e68
Justin Carroll <jcarroll@dyadsoftware.com>
2026-07-12 14:41:29 -0400

    fix: add base path environment variable

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: 'website',
+  base: process.env.VITE_BASE_PATH ?? '/',
   publicDir: path.resolve(__dirname, '.build', 'generated'),
   build: {
     outDir: path.resolve(__dirname, 'dist'),
